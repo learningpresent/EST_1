4943cf1df6fddd2d327ca041916ad3462a87600b
learningpresent <93984397+learningpresent@users.noreply.github.com>
2024-04-29 22:00:14 +0200

    Update Book.java

--- untitled3/src/Book.java
+++ untitled3/src/Book.java
@@ -6,13 +6,15 @@ public class Book {
     String titel;
     String author;
     String year;
+    int id;
 
     public ArrayList<BookCopy>copies=new ArrayList<>();
 
-    public Book(String isbn,String titel,String author,String year){
+    public Book(int id,String isbn,String titel,String author,String year){
         this.isbn=isbn;
         this.titel = titel;
         this.author =author;
         this.year =year;
+        this.id = id;
     }
 }
